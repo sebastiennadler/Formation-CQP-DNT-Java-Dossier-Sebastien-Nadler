d1501df58dc6b3311e26276254d193261b4c5897
Sébastien Nadler <sebastien.nadler@gmail.com>
2015-10-26 15:46:31 +0100

    mise à jour avec dossiers

---
+++ CVPhp/cible.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css"/>
+		<title>Sébastien Nadler </title>
+    </head>
+
+	<body>
+        <p><?php echo $_POST['commentaire'];?></p>
+	</body>
+
+
+</html>
